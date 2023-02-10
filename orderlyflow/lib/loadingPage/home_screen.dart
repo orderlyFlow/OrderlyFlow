390dad95c4c5aa4f5506fd2159235ac17bcaf854
Alaa <chami.alaa321@gmail.com>
2023-02-10 20:07:18 +0200

    ADD: loadingPage

---
+++ orderlyflow/lib/loadingPage/home_screen.dart
@@ -0,0 +1,104 @@
+//ignore_for_file: depend_on_referenced_packages
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:orderlyflow/palette.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  // ignore: library_private_types_in_public_api
+  // ignore: prefer_const_constructors
+  // ignore: prefer_const_literals_to_create_immutables
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Paletter.mainBgLight,
+      body: Column(children: [
+        Center(
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(0, 55, 0, 0),
+            child: Image.asset(
+              'assets/images/logo.png',
+              width: 250,
+              height: 260,
+              alignment: Alignment.center,
+            ),
+          ),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 70.0,
+            ),
+            const Text(
+              "O",
+              style: TextStyle(
+                fontSize: 55,
+                color: Colors.white,
+                fontFamily: 'neuropol',
+              ),
+            ),
+            // ignore: prefer_const_constructors
+            // SizedBox(
+            // height: 25.0,
+            //  ),
+            // ignore: prefer_const_constructors
+            const Text(
+              "derly  Flow",
+              // ignore: prefer_const_constructors
+              style: TextStyle(
+                fontSize: 55,
+                color: Paletter.mainBg,
+                fontFamily: 'neuropol',
+              ),
+            ),
+          ],
+        ),
+        // ignore: prefer_const_constructors
+        SizedBox(
+          height: 50,
+        ),
+        Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ListTile(
+                  contentPadding: const EdgeInsets.fromLTRB(522, 0, 0, 20),
+                  isThreeLine: false,
+                  leading: LoadingAnimationWidget.newtonCradle(
+                    // ignore: prefer_const_constructors
+                    color: Paletter.mainBg,
+                    size: 300,
+                  )),
+              const Text(
+                "loading",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Paletter.mainBg,
+                  fontFamily: 'neuropol',
+                ),
+              ),
+            ]),
+        // ignore: prefer_const_constructors
+        SizedBox(
+          height: 70.0,
+        ),
+        // ignore: prefer_const_constructors
+
+        // ignore: prefer_const_constructors
+        // SizedBox(
+        // height: 25.0,
+        //  ),
+        // ignore: prefer_const_constructor
+      ]),
+    );
+  }
+}
--- orderlyflow/lib/main.dart
+++ orderlyflow/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'LogIn/log_in.dart';
-import 'LogIn/log_in.dart';
+import 'loadingPage/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => LogIn(),
+      '/': (context) => HomeScreen(),
     },
   ));
 }
